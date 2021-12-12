729fba2ea3244be8529e3a33772d29e1e84a4d6e
suman2662021 <sumanbale1110@gmail.com>
2021-12-12 22:13:54 +0530

    yml 2  file added

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send(`Server running at ${port} (Deployed via workflow with secret)`)
+    res.send(`Server running at ${port}`)
 })
 
 app.use("/api/v1/auth", authRoutes);
